ae2d954ec1a52e1c2e88f74e08265ad2d40c85c5
alexc9805 <76743758+brayanc9805@users.noreply.github.com>
2025-01-16 14:23:26 -0500

    Se crea vista par login y menu

--- API/APISV/SV.API/Program.cs
+++ API/APISV/SV.API/Program.cs
@@ -4,7 +4,7 @@ namespace SV.API
 {
     public class Program
     {
-        //TODo empezar video 9: https://www.youtube.com/watch?v=xbPsxE8lm4U&list=PLx2nia7-PgoA1-y-qrxCQii0-EmC4JZ5e&index=9
+        //TODo empezar video 10: https://www.youtube.com/watch?v=M9VcJWyk1co&list=PLx2nia7-PgoA1-y-qrxCQii0-EmC4JZ5e&index=10
 
         public static void Main(string[] args)
         {
--- API/APISV/SV.API/obj/Debug/net8.0/SV.API.AssemblyInfo.cs
+++ API/APISV/SV.API/obj/Debug/net8.0/SV.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SV.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d626fda2e754d5ecbb47b96c9ad2aa36e4a0765c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89854de5bf5fc17b07935611866a216c7fade6e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SV.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SV.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
